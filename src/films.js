5a8489d4ed4fd3b1d0a1b746fdf7fc8640af5668
Natalia <natarose727@gmail.com>
2025-11-17 12:34:24 +0100

    Feat: Exercise 1 completed - Implementation of getAllDirectors

--- src/films.js
+++ src/films.js
@@ -1,7 +1,9 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result = array.map(function(movie){
+    return movie.director;
+  });
+  console.log("Exercise 1 ->", result);
   return result;
 }
 
